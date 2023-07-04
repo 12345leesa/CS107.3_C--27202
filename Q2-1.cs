a142fa0034f820a50c4e859d98eeec120c317d6c
12345leesa <138576274+12345leesa@users.noreply.github.com>
2023-07-04 22:36:45 +0530

    Add files via upload

---
+++ Q2-1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q2_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" enter the first number");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("enter the second number");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+
+            int sum = number1 + number2;
+
+            Console.WriteLine("sum is " +sum);
+
+            Console.ReadLine();
+        }
+    }
+}
