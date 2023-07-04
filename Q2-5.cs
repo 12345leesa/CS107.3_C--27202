afd08c097552cd35000dc23457bf3386c003c5dd
12345leesa <138576274+12345leesa@users.noreply.github.com>
2023-07-04 23:15:12 +0530

    Add files via upload

---
+++ Q2-5.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q2_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for( int i =1; i <=10; i++)
+            {
+                Console.WriteLine("enter the number");
+                int number = Convert.ToInt32(Console.ReadLine());
+
+                if (number== 0 )
+                {
+                    Console.WriteLine("number is zero");
+                }
+                else if (number%2 == 0)
+                {
+                    Console.WriteLine("number is even");
+                }
+                else
+                {
+                    Console.WriteLine("number is odd");
+                }
+                Console.ReadLine();
+                   
+            }
+        }
+    }
+}
