80bd2c36bd831e1542bdd95a92450bbd1ad8e0d8
12345leesa <138576274+12345leesa@users.noreply.github.com>
2023-07-04 23:20:33 +0530

    Add files via upload

---
+++ Q3-1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q3_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" enter an integer");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number%2 == 0)
+            {
+                Console.WriteLine("even");
+
+            }
+            else
+            {
+                Console.WriteLine("odd");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
