29758c2915142d15f948e6d611792f3e43cb5c11
12345leesa <138576274+12345leesa@users.noreply.github.com>
2023-07-04 22:12:02 +0530

    Add files via upload

---
+++ Q1-2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter  the  radius of the circle");
+            double redius = Convert.ToDouble(Console.ReadLine());
+
+            double area = CalculateCircleArea(radius);
+
+            Console.WriteLine("the area of the circle is " +area);
+            Console.ReadLine();
+
+
+        }
+
+        static double CalculateCircleArea(double radius)
+        {
+
+            double area = Math.PI * radius * radius;
+            return area;
+
+        }
+    }
+}
