75eedc2889f8d22058db897b4cbad5174a867766
12345leesa <138576274+12345leesa@users.noreply.github.com>
2023-07-04 22:46:01 +0530

    Add files via upload

---
+++ Q2-2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q2_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" enter the first number");
+            double number1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(" enter the second number");
+            double number2 = Convert.ToDouble(Console.ReadLine());
+
+            double sum = number1 + number2;
+            double sub = number1 - number2;
+            double mul = number1 * number2;
+            double divi = number1 - number2;
+
+            Console.WriteLine(" sum is " +sum);
+            Console.WriteLine(" sub is " +sub);
+            Console.WriteLine(" mul is " +mul);
+            Console.WriteLine("divi is " +divi);
+
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
