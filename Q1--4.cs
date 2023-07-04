4146125a264c9d64e1dc80a2389ffd514e00663d
12345leesa <138576274+12345leesa@users.noreply.github.com>
2023-07-04 22:29:05 +0530

    Add files via upload

---
+++ Q1--4.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1__4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" enter the emloyee salary");
+            double salary = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("enter the tax rate(in decimal form )");
+            double taxRate = Convert.ToDouble(Console.ReadLine());
+
+            double salaryAfterTax = salary * (1- taxRate);
+            Console.WriteLine(" salary after tax " +salaryAfterTax);
+
+            Console.ReadLine();
+        }
+    }
+}
