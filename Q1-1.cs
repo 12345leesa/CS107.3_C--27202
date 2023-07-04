c94d8585d0a519e0e8521111576eae6f40430b12
12345leesa <138576274+12345leesa@users.noreply.github.com>
2023-07-04 22:03:45 +0530

    Add files via upload

---
+++ Q1-1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q_1._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the your name");
+            string name = Console.ReadLine();
+
+
+            Console.WriteLine("enter the your batch");
+            string batch = Console.ReadLine();
+
+            Console.WriteLine(" your name is " +name);
+            Console.WriteLine("your batch is " +batch);
+
+            Console.ReadLine();
+        }
+    }
+}
