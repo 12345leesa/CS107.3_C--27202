b89abfa7089827d863c63d6f783e3b9dfa06043a
12345leesa <138576274+12345leesa@users.noreply.github.com>
2023-07-05 22:34:30 +0530

    Add files via upload

---
+++ lab6 Q6.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6_Q6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enteer the size of the array");
+            int size = Convert.ToInt32(Console.ReadLine());
+
+            ArrayOperations arrayOps = new ArrayOperations();
+            arrayOps.FillArray(size);
+
+            Console.ReadLine();
+        }
+    }
+}
