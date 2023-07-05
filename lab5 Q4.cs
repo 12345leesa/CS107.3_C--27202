b2ba1526bb6b532474e17e7586ffdde50f499cbb
12345leesa <138576274+12345leesa@users.noreply.github.com>
2023-07-05 21:40:07 +0530

    Add files via upload

---
+++ lab5 Q4.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5_Q4
+{
+    internal class SeparateClass
+    {
+        private void SayHello()
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
