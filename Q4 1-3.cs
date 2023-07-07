b794cc62318ff71e892d1dbae09aafdadaf1de48
12345leesa <138576274+12345leesa@users.noreply.github.com>
2023-07-07 23:44:42 +0530

    Add files via upload

---
+++ Q4 1-3.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q4_1_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the kilometer value");
+            double kilometer = Convert.ToDouble(Console.ReadLine());
+
+            ConvertValues converter = new ConvertValues();
+            double meter = converter.kilometetToMeter(kilometer);
+
+            Console.WriteLine("equeation meter value is " +meter);
+
+            Console.ReadLine();
+        }
+    }
+}
