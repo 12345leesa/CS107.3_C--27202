c63e66333ac0c609400771e23e6703bbe2a5567a
12345leesa <138576274+12345leesa@users.noreply.github.com>
2023-07-07 19:54:33 +0530

    Add files via upload

---
+++ lab6 Q8.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab6_Q8
+{
+    internal class Animal
+    {
+        public void PrintAnimal()
+        {
+            Console.WriteLine(" i am an animal");
+        }
+    }
+}
