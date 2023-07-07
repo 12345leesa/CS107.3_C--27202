269b507c5635c44504572c607747578b98ec3e13
12345leesa <138576274+12345leesa@users.noreply.github.com>
2023-07-07 23:13:13 +0530

    Add files via upload

---
+++ Q4 1-1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q4__1._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConvertValues converter = new ConvertValues();
+            Convert.kiloToMeter();
+
+            Console.ReadLine();
+        }
+    }
+}
