afce66616dcb66968a0484416ae642d97781d8f5
12345leesa <138576274+12345leesa@users.noreply.github.com>
2023-07-11 09:52:33 +0530

    Add files via upload

---
+++ Encapsulation Q1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            Console.WriteLine("enter employee ID");
+            var empID = int.Parse(Console.ReadLine());
+
+            employee.EmpID = empID;
+
+            Console.WriteLine("enter employee name");
+            string empName = Console.ReadLine();
+            employee.EmpName = empName;
+
+            Console.WriteLine("enter employee Designation");
+            string empDesignation = Console.ReadLine();
+            employee.EmpDesignation = empDesignation;
+
+            Console.WriteLine("hello, {0}! you Employee ID is {1} and you are a {2}", employee.EmpName, employee.EmpID, arg2: employee.EmpDesignation);
+            Console.ReadLine();
+
+        }
+    }
+}
